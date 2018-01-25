a2ce2aeff1ffe32eb56d61c304b7dc3b3cdfd969
axel.em.olsson@gmail.com <axel.em.olsson@gmail.com>
2018-01-25 01:47:07 +0100

    BN script

--- BinaryNinja-scripts/nop-it.py
+++ BinaryNinja-scripts/nop-it.py
@@ -1,6 +1,6 @@
 # NOPs reapeted occurances of MOV al, const.
 # Alternative to IDA Pro script showed in https://www.youtube.com/watch?v=HfSQlC76_s4&feature=youtu.be
-# Binaryninja does not support collapsing parts of the code which is why we NOP and then look at the code in
+# BinaryNinja does not support collapsing parts of the code which is why we NOP and then look at the code in
 # IL view which optimizes the NOPs away.
 
 from binaryninja import *
